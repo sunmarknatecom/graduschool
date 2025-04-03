c8e54857ef050917150c8d5f91c8fafd4cb02cfb
Mark S. Hong <33437440+sunmarknatecom@users.noreply.github.com>
2025-04-03 13:49:20 +0900

    Update code.py

--- code.py
+++ code.py
@@ -59,10 +59,9 @@ def get_transform_var(ct_obj, nm_obj):
     else:
         nm_start_position = float(nm_file_obj["DetectorInformationSequence"][0]["ImagePositionPatient"].value[2])
     nm_3d_image = nm_file_obj.pixel_array
-    nm_3d_image_transposed = np.transpose(nm_3d_image, (1, 2, 0))
     nm_slice_locations = {}
     nm_slice_thickness = float(nm_file_obj.SliceThickness)
-    num_nm_slices = np.shape(nm_3d_image_transposed)[2]
+    num_nm_slices = np.shape(nm_3d_image)[0]
     for i in range(num_nm_slices):
         nm_slice_locations[i + 1] = float(nm_start_position + i * nm_slice_thickness)
     # CT-NM 정렬 지점 찾기 (NM 시작 인덱스 구하기)
