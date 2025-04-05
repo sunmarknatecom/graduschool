f8035578bb009582f88641e3c3de241df303fb2b
Mark S. Hong <33437440+sunmarknatecom@users.noreply.github.com>
2025-04-06 01:06:55 +0900

    colab code to colab_totalsegmentator.py

--- code.py
+++ code.py
@@ -232,29 +232,3 @@ for elem in idx_list:
     temp_dst_path = os.path.join("D:\\gradustudy\\uploadfiles\\",file_name)
     print(temp_src_filename, temp_dst_path)
     shutil.copyfile(temp_src_filename, temp_dst_path)
-
-
-
-## colab
-
-!pip install pydicom totalsegmentator
-
-import os
-from totalsegmentator.python_api import totalsegmentator
-
-root_path = "/content/drive/MyDrive/gradstudy/nifti_ct"
-dst_path = "/content/drive/MyDrive/gradstudy/result"
-
-file_list = [os.path.join(root_path,elem) for elem in os.listdir(root_path)]
-
-for elem in file_list:
-    temp_dst_path =os.path.join(dst_path,os.path.basename(elem)[:4]+"nifti_label")
-    print(elem)
-    print(temp_dst_path)
-
-for elem in file_list:
-    temp_dst_path =os.path.join(dst_path,os.path.basename(elem)[:4]+"nifti_label")
-    print(elem, "Processing start")
-    print(temp_dst_path)
-    totalsegmentator(elem, temp_dst_path, ml=True, task="total")
-    print("Processing copmplete.")
