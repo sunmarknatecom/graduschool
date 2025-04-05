51fd53cf73669edca19e5c4ecf819c62e6e9fd93
Mark S. Hong <33437440+sunmarknatecom@users.noreply.github.com>
2025-04-06 01:13:04 +0900

    Create image_process.py

---
+++ image_process.py
@@ -0,0 +1,8 @@
+idx = "001"
+
+ct_path, nm_path = get_paths(idx)
+lb_path = "D:\\99gradu\\labels\\"+idx+"\\_nifti_label.nii"
+
+ct_objs = open_CT(ct_path)
+nm_obj = open_NM(nm_path)
+lb_obj = open_LB
