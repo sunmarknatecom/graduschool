2f7edfb59ea1905975f79f57f1b256314fe7f565
Mark S. Hong <33437440+sunmarknatecom@users.noreply.github.com>
2025-04-06 09:50:55 +0900

    Update image_process.py

--- image_process.py
+++ image_process.py
@@ -1,7 +1,7 @@
 idx = "001"
 
 ct_path, nm_path = get_paths(idx)
-lb_path = "D:\\99gradu\\labels\\"+idx+"\\_nifti_label.nii"
+lb_path = "D:\\99gradu\\labels\\"+idx+"_nifti_label.nii"
 
 ct_objs = open_CT(ct_path)
 nm_obj = open_NM(nm_path)
