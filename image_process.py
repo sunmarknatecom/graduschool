bb11e9f28eb6b7c38b413d41c9c6961496b5bbf1
Mark S. Hong <33437440+sunmarknatecom@users.noreply.github.com>
2025-04-06 21:11:04 +0900

    Update image_process.py

--- image_process.py
+++ image_process.py
@@ -73,8 +73,8 @@ red_color_nor_tr_lb_image = to_red_image(color_nor_tr_lb_image)
 out_image = copy.copy(red_color_nor_tr_lb_image)
 out_image[out_image>=1]=255
 
-def only_seg_lb_image(src_lb_image, n = 70):
-    return (src_lb_image == n).astype(np.uint8)*n
+def only_seg_lb_image(src_lb_image, seg_n = 70):
+    return (src_lb_image == n).astype(np.uint8)*seg_n
 
 def find_min_max_index(src_lb_image, seg_n = 70):
     indices = np.argwhere(tr_lb_image == seg_n)
