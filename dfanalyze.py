eb4739ae55af743a59c34ddd11c877e9847eccb1
Mark S. Hong <33437440+sunmarknatecom@users.noreply.github.com>
2025-06-15 00:11:16 +0900

    Update dfanalyze.py

--- dfanalyze.py
+++ dfanalyze.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from scipy.stats import shapiro
 
 file_list = os.listdir()
 df_list = []
@@ -11,12 +12,41 @@ for file_path in file_list:
 
 total_df = pd.concat(df_list, ignore_index = True)
 
-RT_SKULL_DF = total_df[total_df['Contour'].str.startswith('Rt_s')]
-LT_SKULL_DF = total_df[total_df['Contour'].str.startswith('Lt_s')]
-RT_HUMERUS_DF = total_df[total_df['Contour'].str.startswith('Rt._h')]
-LT_HUMERUS_DF = total_df[total_df['Contour'].str.startswith('Lt_h')]
-RT_FEMUR_DF = total_df[total_df['Contour'].str.startswith('Rt_f')]
-LT_FEMUR_DF = total_df[total_df['Contour'].str.startswith('Lt_f')]
+RT_SKL_DF = total_df[total_df['Contour'].str.startswith('Rt_s')]
+LT_SKL_DF = total_df[total_df['Contour'].str.startswith('Lt_s')]
+RT_HUM_DF = total_df[total_df['Contour'].str.startswith('Rt._h')]
+LT_HUM_DF = total_df[total_df['Contour'].str.startswith('Lt_h')]
+RT_FEM_DF = total_df[total_df['Contour'].str.startswith('Rt_f')]
+LT_FEM_DF = total_df[total_df['Contour'].str.startswith('Lt_f')]
 
-METS = total_df[total_df['Contour'].str.startswith('Met')]
-DEGS = total_df[total_df['Contour'].str.startswith('Deg')]
+METS_DF = total_df[total_df['Contour'].str.startswith('Met')]
+DEGS_DF= total_df[total_df['Contour'].str.startswith('Deg')]
+
+COL_INDEX = total_df.columns
+
+# COL_INDEX=
+"""
+00 Contour
+01 Finding
+02 Series Date
+03 Integral Total (SUVbw*ml)
+04 Kurtosis (-)
+05 Max (SUVbw)
+06 Mean (SUVbw)
+07 Median (SUVbw)
+08 Min (SUVbw)
+09 Skewness (-)
+10 Slice with Maximum Value (#)
+11 Sphere Diameter (cm)
+12 Standard Deviation (SUVbw)
+13 Total (SUVbw)
+14 Volume (ml)
+15 Index_Number
+"""
+
+RT_SKL_MAX_MAX  = RT_SKL_DF["Max (SUVbw)"].max()
+RT_SKL_MAX_MEAN = RT_SKL_DF["Max (SUVbw)"].mean()
+RT_SKL_MAX_STD  = RT_SKL_DF["Max (SUVbw)"].std()
+
+print(RT_SKL_MAX_MAX, RT_SKL_MAX_MEAN, RT_SKL_MAX_STD)
+
